f3f22deb83dbe0a17ff02118426e081679cb57ae
Vinicius Macedo <viniciusmcode@gmail.com>
2023-08-17 15:27:08 -0300

    primeiro commit

---
+++ trabalho1/funcoes.hpp
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<iostream>
+#include<cstdlib>
+#define TAM 3
+
+using namespace std;
+
+void printLista(int lista[]){
+	for(unsigned i=0; i<TAM; i++){
+		cout << lista[i] << " ";
+	}
+		cout << endl;
+}
+
+int* randomLista(int lista[]){
+	
+	for(unsigned i=0; i<TAM; i++){
+		lista[i] = rand()%100;
+	}
+	
+	return lista;
+}
+
+
+int* lerLista(int lista[], int quant){
+	
+	for(unsigned i=0; i<quant; i++){
+		cin >> lista[i];
+	}
+
+}
+
+int buscaValor(int lista[], int val){
+	
+	for(unsigned i=0; i<TAM; i++){
+		if(val == lista[i])
+			return i;
+	}
+	cout << "Valor nao encontrado" << endl;
+}
+
+void inserePosicao(int lista[], int pos){
+	if(pos>TAM || pos<0){
+		cout << "Posicao nao encontrada!!" << endl;
+	}
+
+}
---
+++ trabalho1/trabalho1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cstdlib>
+#include"funcoes.hpp"
+
+using namespace std;
+
+void printLista(int lista[]);
+int* randomLista(int lista[]);
+int* lerLista(int lista[], int quant);
+int buscaValor(int lista[], int val);
+void inserePosicao(int lista[], int pos);
+
+int main(){
+	int lista[TAM],opcao,quant;
+	
+	
+	randomLista(lista);
+	lerLista(lista);
+	printLista(lista);
+	buscaValor(lista,2);
+	inserePosicao(lista,3);
+	
+	
+	cin >> opcao;
+	switch(opcao){
+		case 1:{
+			cin >> quant;
+			lerLista(lista, quant);
+			break;
+		} 
+	}
+	
+	
+	return 0;
+}
