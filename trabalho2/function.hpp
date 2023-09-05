af7698b4eed50b424bdb0f6998297f7efc5c97ee
Vinicius Macedo <viniciusmcode@gmail.com>
2023-09-05 12:50:26 -0300

    finalizando e organizando codigo

---
+++ trabalho2/function.hpp
@@ -0,0 +1,90 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//*************************************************IMPRIMET**************************************************************
+
+void imprime(vector<int> vec){
+  for(long unsigned int i=0; i<vec.size(); i++)
+      cout << vec[i] << " ";
+    cout << endl;
+}
+
+//**********************************************INSERTION SORT***********************************************************
+
+void insertionSort(vector<int> vec, int tam, bool aleatorio){
+    
+    int comp = 0;
+    int i, valor, j;
+    for (i = 1; i < tam; i++) {
+        valor = vec[i];
+        j = i - 1;
+        while (j >= 0 && vec[j] > valor) {
+            comp+=2;
+            vec[j + 1] = vec[j];
+            j = j - 1;
+        }
+        vec[j + 1] = valor;
+        comp++;
+        if(!aleatorio)
+            imprime(vec);  // imprime repetidamente
+    }
+    comp = comp + (tam-1);
+    if(aleatorio)  // imprime uma só vez
+        imprime(vec);
+    cout << "Comparações: " << comp << endl << "--------------------------------------------------------------" << endl;
+}
+
+//**********************************************SELECTION SORT***********************************************************
+
+void selectionSort(vector<int> vec, int tam, bool aleatorio){
+
+    int comp = 0;
+    int idx = 0;
+    for(int i=0; i<tam; i++){
+        idx = i;
+        for(int j=i+1; j<tam; j++){
+            if(vec[idx] > vec[j])
+                idx = j;   
+        }
+        // swap
+        int aux = vec[i];
+        vec[i] = vec[idx];
+        vec[idx] = aux;
+        
+        comp += (tam-i-1);  // acessos
+        if(!aleatorio)
+            imprime(vec);  // imprime repetidamente
+    }
+    if(aleatorio)  // imprime uma só vez
+        imprime(vec);
+    cout << "Comparações: " << comp << endl << "--------------------------------------------------------------" << endl;
+}
+
+//**********************************************GERA ALEATORIA***********************************************************
+
+void criaAleatoria(bool opcao){
+    
+    vector<int> vAle;
+    if(opcao==true){
+
+        for(int i=1; i<=10; i++){
+            for(int j=1; j<=i; j++){
+                vAle.push_back((rand() % 20) + 1);
+            }
+            imprime(vAle);
+            selectionSort(vAle, vAle.size(), true);
+            vAle.clear();
+        }
+    }else{
+
+        for(int i=1; i<=10; i++){
+            for(int j=1; j<=i; j++){
+                vAle.push_back((rand() % 20) + 1);
+            }
+            imprime(vAle);
+            insertionSort(vAle, vAle.size(), true);
+            vAle.clear();
+        }
+    }
+}
--- trabalho2/main.cpp
+++ trabalho2/main.cpp
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<vector>
 #include<time.h>
+#include"function.hpp"
 
 using namespace std;
 
@@ -10,62 +11,32 @@ using namespace std;
 
 void imprime(vector<int> vec);
 void selectionSort(vector<int> vec, int tam, bool aleatorio);
-void criaAleatoria();
+void criaAleatoria(bool opcao);
 
 int main() {
     // srand(time(NULL));  //descometar isso
+    vector<int> vec = {70, 1, 12, 8, 99, 72, 5, 15, 20, 91, 14, 61, 66, 41, 81, 88, 16, 21, 34, 90};
 
+    //**********************************************SELECTION SORT***********************************************************
+    cout << endl << "************************SELECTION SORT************************" << endl << endl;
     // lista unica
-    vector<int> vec = {70, 5, 12, 8, 99, 72, 1, 15, 20, 91, 14, 61, 66, 41, 81, 88, 16, 21, 34, 90};
-
+    cout << ">> LISTA PRINCIPAL" << endl;
     imprime(vec);
     selectionSort(vec, TAM, false);
 
     //lista aleatoria
-    criaAleatoria();
+    cout << endl << ">> LISTA ALEATORIA" << endl;
+    criaAleatoria(true);
 
-}
+    //**********************************************INSERTION SORT***********************************************************
+    cout << endl << "************************INSERTION SORT************************" << endl << endl;
+    // lista unica
+    cout << ">> LISTA PRINCIPAL" << endl;
+    imprime(vec);
+    insertionSort(vec, TAM, false);
 
-void imprime(vector<int> vec){
-  for(int i=0; i<vec.size(); i++)
-      cout << vec[i] << " ";
-    cout << endl;
-}
-
-void selectionSort(vector<int> vec, int tam, bool aleatorio){
-
-    int cont = 0;
-    int idx = 0;
-    for(int i=0; i<tam; i++){
-        idx = i;
-        for(int j=i+1; j<tam; j++){
-            if(vec[idx] > vec[j])
-                idx = j;   
-        }
-        // swap
-        int aux = vec[i];
-        vec[i] = vec[idx];
-        vec[idx] = aux;
-        
-        cont += (tam-i-1);  // acessos
-        if(!aleatorio)
-            imprime(vec);  // imprime repetidamente
-    }
-    if(aleatorio)  // imprime uma só vez
-        imprime(vec);
-    printf("Comparacoes %d\n", cont);
-}
-
-void criaAleatoria(){
-    
-    vector<int> vAle;
-
-    for(int i=1; i<=10; i++){
-        for(int j=1; j<=i; j++){
-            vAle.push_back((rand() % 20) + 1);
-        }
-        imprime(vAle);
-        selectionSort(vAle, vAle.size(), true);
-        vAle.clear();
-    }
+    //lista aleatoria
+   cout << endl << ">> LISTA ALEATORIA" << endl;
+   criaAleatoria(false);
+   
 }
