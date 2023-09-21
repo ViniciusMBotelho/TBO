c28bbc285fc02fb9a84a043a5f811bf0437ed93f
gabrielramos731 <ramos.gabrielsilva23@gmail.com>
2023-09-21 13:51:56 -0300

    leitura do arquivo

--- Projeto 1/main.cpp
+++ Projeto 1/main.cpp
@@ -1,24 +1,67 @@
 #include<iostream>
 #include<stdlib.h>
 #include<fstream>
+#include <sstream>
 #include<vector>
 #include<string>
+#include <iostream>
+#include <algorithm>
+#include "Cinemas.hpp"
 
 using namespace std;
 
+string removeSpaces(string str){
+    str.erase(0, 1);
+    return str;
+}
+
 int main(){
-    ifstream cinemas;
-    cinemas.open("dados/cinemas.txt");
-    
-    if(cinemas.is_open())
-        cout << "b";
-    else
-        cout << "!!!";
-    getchar();
+    ifstream cinemasArq;
+    cinemasArq.open("dados/cinemas.txt");
+
+    vector<Cinema> cinemas;
+    Cinema cinema;
+
     string linha;
-    while(!cinemas.eof()){
-        // getline(cinemas, linha);
-        // cout << linha << endl;
-        // cout << "a";
-    }
+    string seg;
+    vector<string> seglist;
+    getline(cinemasArq, linha);
+
+    int idxCol = 0;
+    while(getline(cinemasArq, linha)){
+        stringstream teste(linha);
+        while(getline(teste, seg, ',')){
+            seg = removeSpaces(seg);
+            switch (idxCol){
+            case 0:
+                cinema.setCinema_id(seg);
+                break;
+            case 1:
+                cinema.setNome(seg);
+                cout << (seg);
+                break;
+            case 2:
+                cinema.setCord_x(stoi(seg));
+                break;
+            case 3:
+                cinema.setCord_y(stoi(seg));
+                break;
+            case 4:
+                // cinema.setPreco(stof(seg));
+                break;
+            default:
+                if((seg)[0] == 'c'){
+                    idxCol = -1;
+                    break;
+                }
+                else
+                    cinema.setFilmes_exibicao(seg);
+            }
+            idxCol++;
+            // seglist.push_back(removeSpaces(seg));
+        }
+        cinemas.push_back(cinema);
+        // cout << cinema.getCinema_id();
+        // cout << "!!!";
+    }    
 }
